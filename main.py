02e48194648051ef748ab69c22d9f34cf3722ff0
Victor Bejarano <alejandrobjrn4@gmail.com>
2019-01-30 23:17:51 -0500

    Calculo de las distancias
    Calculo de las distancias de los cuadros entre frames

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ Created on Wed Jan 30 20:09:45 2019
 @author: victo
 """
 import cv2
+import math
+import time
 #Cargar Video
 camara = cv2.VideoCapture("./video/BANDA_AGUA.mp4")
 # Variables
 fondo = None #Nos servirá para obtener el fondo
 PosicionQ=[] #Posiciones de los Cuadros
 PosicionQ2=[] #Posiciones de los Cuadros externos
+PosicionQ3=[]
 tamanoRecX = 100
 tamanoRecY = 0
 condicion = 0
+vel_avance = 100 #velocidad de avance de la banda en m/s
+longitudCam = 0.5 #longitud de la camara en m
 #Reproduccion de video
+tiempo_in = time.time()
+tiempo_frame = 0
 while True:
     #Obtiene el frame
     (grabbed, frame) = camara.read()
     #Finaliza el ciclo cuando termina el video
     if not grabbed:
         break
+    K = longitudCam / frame.shape[1] #Constante de Relacion entre longitud de la imagen y del area m/pixel
     # Convertimos a escala de grises
     gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Aplicamos suavizado para eliminar ruido
@@ -69,18 +77,31 @@ while True:
         #se copian los rectangulos que son externos
         if condicion == 0:
             PosicionQ2 = PosicionQ2 + [PosicionQ[e]]
+            centroX = PosicionQ[e][0] + int(PosicionQ[e][2]/2)
+            centroY = PosicionQ[e][1] + int(PosicionQ[e][3]/2)
+            if len(PosicionQ3) > 0:
+                for j in PosicionQ3:
+                    PuntoX = j[0] + int(j[2]/2)
+                    PuntoY = j[1] + int(j[3]/2)
+                    radioM = math.sqrt(pow(centroX - PuntoX,2)+pow(centroY - PuntoY,2))
+                    print(radioM * K / tiempo_frame)
+                    #####Continuar con la idea de saber cual cuadro corresponde con el frame siguiente
         condicion = 0
     for f in PosicionQ2:
         # Dibujamos el rectángulo del bounds
         centroX=f[0] + int(f[2]/2)
         centroY=f[1] + int(f[3]/2)
         cv2.rectangle(frame, (f[0], f[1]), (f[0] + f[2], f[1] + f[3]), (0, 0, 255), 2)
+    PosicionQ3 = PosicionQ2
     PosicionQ = []
     PosicionQ2 = []
     condicion = 0
+    tiempo_frame = time.time() - tiempo_in
+    tiempo_in = time.time()
     #Visualiza el video
     cv2.imshow("Camara", frame)
     cv2.imshow("Resta", resta)
+
     # Capturamos una tecla para salir
     key = cv2.waitKey(1) & 0xFF
     # Si ha pulsado la letra s, salimos
